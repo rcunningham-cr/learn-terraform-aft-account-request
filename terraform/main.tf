c9628016de5981bdc33bfa02855b9e3aaa46c755
rcunningham-cr <richard.cunningham@cloudreach.com>
2022-07-05 09:42:25 +0100

    Adding account

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,38 +1,9 @@
-module "sandbox" {
+module "sandboxerino" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "me+sandboxtest@richcunningham.uk"
-    AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Sandbox"
-    SSOUserEmail              = "rich@cunningham.sh"
-    SSOUserFirstName          = "Sandbox"
-    SSOUserLastName           = "AFT"
-  }
-
-  account_tags = {
-    "Learn Tutorial" = "AFT"
-  }
-
-  change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
-  }
-
-  custom_fields = {
-    vpc_name = "test"
-    vpc_cidr = "192.168.69.0/24"
-  }
-
-  account_customizations_name = "sandbox"
-}
-
-module "sandbox1" {
-  source = "./modules/aft-account-request"
-
-  control_tower_parameters = {
-    AccountEmail              = "me+sandboxtest1@richcunningham.uk"
-    AccountName               = "sandbox-aft1"
+    AccountEmail              = "me@richcunningham.uk"
+    AccountName               = "sandboxing"
     ManagedOrganizationalUnit = "Sandbox"
     SSOUserEmail              = "rich@cunningham.sh"
     SSOUserFirstName          = "Sandbox"
